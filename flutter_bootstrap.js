6be9d4ca7c4e22e3297b6fbc377245235d4d72e2
PhuongMinhVN <PhuongMinhVN@users.noreply.github.com>
2026-01-05 11:39:50 +0000

    deploy: f4655716968422258ac28ed159ac12b38c4ef471

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2653486820"
+    serviceWorkerVersion: "3888596830"
   }
 });
